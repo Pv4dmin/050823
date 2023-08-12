07486c70a5ac07385ff67bc22c949c657ae316a6
Vyacheslav <pvadmin@mail.ru>
2023-08-12 13:24:26 +0800

    HWork1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//Программа которая на вход принимает два числа и выдает какое число большее, а какое меньшее
+
+Console.WriteLine("Введите число 1");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int number2 = int.Parse(Console.ReadLine());
+if(number1 > number2)
+{
+Console.WriteLine($"Число {number1} больше числа {number2}");
+}
+else
+{
+Console.WriteLine($"Число {number2} больше числа {number1}");
+}
